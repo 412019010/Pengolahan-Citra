e43760d9ff57bc5ba18c1d3c50f949d416ee8476
Ivan Christian <ivan.412019010@civitas.ukrida.ac.id>
2022-05-12 22:41:18 +0700

    a

--- humandetection.py
+++ humandetection.py
@@ -40,14 +40,8 @@ def humanDetector(args):
     writer = None
     if args['output'] is not None and image_path is None:
         writer = cv2.VideoWriter(args['output'],cv2.VideoWriter_fourcc(*'MJPG'), 10, (600,600))
-
-    if camera:
-        print('[INFO] Opening Web Cam.')
-        detectByCamera(ouput_path,writer)
-    elif video_path is not None:
-        print('[INFO] Opening Video from path.')
-        detectByPathVideo(video_path, writer)
-    elif image_path is not None:
+    
+    if image_path is not None:
         print('[INFO] Opening Image from path.')
         detectByPathImage(image_path, args['output'])
 
